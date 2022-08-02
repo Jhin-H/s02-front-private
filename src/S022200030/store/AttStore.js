a278f2c8f2759597a3125f448d74b6c5a118f054
황어진 <lawliet9712@gmail.com>
2022-08-02 13:39:44 +0900

    commit

---
+++ src/S022200030/store/AttStore.js
@@ -0,0 +1,17 @@
+import {observable, action, computed, makeObservable} from 'mobx';
+
+
+const AttStore = () => {
+    
+
+    return (
+        <div>
+            
+        </div>
+
+
+        
+          
+    );
+}
+export default AttStore;
