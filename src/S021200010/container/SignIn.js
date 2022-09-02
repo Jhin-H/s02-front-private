1ef80a402440f2aaec8e407a3d495aaf250bc2e2
황어진 <lawliet9712@gmail.com>
2022-09-02 18:24:48 +0900

    로그인 커밋

--- src/S021200010/container/SignIn.js
+++ src/S021200010/container/SignIn.js
@@ -117,7 +117,6 @@ const SignIn = observer(() => {
                     placeholder="이메일을 입력해주세요"
                     onChange={onChangeEmail}/>
                     
-                    
                     <LoginInput className="pwForm"
                     type="password"
                     placeholder="비밀번호를 입력해주세요"
